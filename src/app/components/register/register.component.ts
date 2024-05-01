62313a07dc3a0a02400e4f2904026d91375dea8e
Diego Flores <floresdiego041@gmail.com>
2024-04-30 22:03:42 -0600

    Agregando la funcion de registrar usuarios

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { User } from '../../models/User.model';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'register-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  userRegistered = new User();
+  usersService = inject(UsersService);
 
+  async saveUser(registerForm:NgForm){
+    const response = await this.usersService.register(JSON.stringify(this.userRegistered).toString())
+    console.log(response);
+    alert(`Registro Existoso\nBienvenido ${this.userRegistered.name}`);
+    registerForm.resetForm();
+  }
 }
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  private httpClient = inject(HttpClient);
+  private url: String;
+
+  constructor() {
+    this.url = 'http://localhost:8080/api/v1/users';
+  }
+
+  register(user: any){
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+    return firstValueFrom(this.httpClient.post<any>(`${this.url}/add`, user, httpOptions));
+  }
+}
