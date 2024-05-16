435ef04165dcf422b192d9e36cd315bbe0bcdc4f
juansolm <juandsolism@gmail.com>
2024-05-15 18:34:05 -0600

    Modificando vistas del cuestionario inicial

--- src/app/components/genre-survey/genre-survey.component.ts
+++ src/app/components/genre-survey/genre-survey.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faGun } from '@fortawesome/free-solid-svg-icons';
-import { faSkull } from '@fortawesome/free-solid-svg-icons';
-import { faChess } from '@fortawesome/free-solid-svg-icons';
 import { faCar } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faSkull } from '@fortawesome/free-solid-svg-icons';
 import { faShieldHalved } from '@fortawesome/free-solid-svg-icons';
-import { faHandBackFist } from '@fortawesome/free-solid-svg-icons';
+import { faPersonDigging } from '@fortawesome/free-solid-svg-icons';
+import { faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
+import { faChessBoard } from '@fortawesome/free-solid-svg-icons';
+import { faSoccerBall } from '@fortawesome/free-solid-svg-icons';
+import { faMap } from '@fortawesome/free-solid-svg-icons';
+import { faPeopleRoof } from '@fortawesome/free-solid-svg-icons';
+import { faCube } from '@fortawesome/free-solid-svg-icons';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
+import { faCampground } from '@fortawesome/free-solid-svg-icons';
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
+import { faDice } from '@fortawesome/free-solid-svg-icons';  
+import { faGun } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-genre-survey',
@@ -17,10 +29,21 @@ import { faHandBackFist } from '@fortawesome/free-solid-svg-icons';
 })
 
 export class GenreSurveyComponent {
-  faGun = faGun;
-  faSkull = faSkull;
-  faChess = faChess;
   faCar = faCar;
+  faHeart = faHeart;
   faShieldHalved = faShieldHalved;
-  faHandBackFist = faHandBackFist;
+  faSkull = faSkull;
+  faPersonDigging = faPersonDigging;
+  faPuzzlePiece = faPuzzlePiece;
+  faCoins = faCoins;
+  faChessBoard = faChessBoard;
+  faSoccerBall = faSoccerBall;
+  faMap = faMap;
+  faPeopleRoof = faPeopleRoof;
+  faCube = faCube;
+  faBook = faBook;
+  faCampground = faCampground;
+  faLightbulb = faLightbulb;
+  faDice = faDice;
+  faGun = faGun;
 }
